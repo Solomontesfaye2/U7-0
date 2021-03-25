fec1bb6ea50f5978dd3c68b9aa38a4a1424c9cec
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-25 18:10:04 +0000

    techawtebgn

--- script.js
+++ script.js
@@ -5,7 +5,5 @@ document.getElementById('enter').addEventListener('click', enter)
 function enter () {
   day = document.getElementById('input').value
   if (day === 'Monday' || day === 'Thusday' || day === 'Wensday' || day === 'Thursday' || day === 'Friday')
-    alert('Time for school!') } day = document.getElementById('input')
-       .value
- }
+    alert('Time for school!') } day = document.getElementById('input').value
 }  
